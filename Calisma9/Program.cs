99bb38b5a2a62acc8a3cb462ee64ade4ff02ae9b
aleynazengin <aleystyles23@gmail.com>
2021-06-02 19:30:25 +0300

    Calismalar eklendi

---
+++ Calisma9/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Calisma9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //While
+            //1 den başlayarak consoledan girilen sayıya kadar (sayı dahil) ortalama hesaplayıp console a yazdıran program.
+            Console.Write("Lütfen bir sayı giriniz: ");
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac= 1;
+            int toplam =0;
+            while (sayac<= sayi)
+            {
+                toplam += sayac;
+                sayac++;
+
+            }
+           Console.WriteLine(toplam/sayi);
+
+           // 'a'dan 'z' ye kadar tüm harfleri console a yazdır.
+           char character = 'a';
+           while (character<'z')
+           {
+               Console.Write(character);
+               character++;
+           }
+
+           Console.WriteLine("***** Foreach *****");
+           string [] arabalar = {"BMW","Ford","Toyota","Nissan"};
+
+           foreach (var araba in arabalar)
+           {
+               Console.WriteLine(araba);
+           }
+        }
+    }
+}
