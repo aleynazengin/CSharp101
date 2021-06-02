53739801db30e2a61c17b366e61935512d7b8bd5
aleynazengin <aleystyles23@gmail.com>
2021-06-02 19:50:11 +0300

    Tanımlama, Erisim ve Dongulerle Dizi Kullanımı

---
+++ Calisma10/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Calisma10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dizi Tanımlama
+            string [] renkler = new string [5];
+            string [] hayvanlar = {"Kedi","Köpek","Tavşan","Kuş"};
+
+            int[] dizi;
+            dizi = new int [5];
+
+            //Dizilere Değer Atama ve Erişim
+            renkler[0]= "Mavi";
+            dizi[3]=10;
+
+            Console.WriteLine(hayvanlar[1]);
+            Console.WriteLine(dizi[3]);
+            Console.WriteLine(renkler[0]);
+
+            //Döngülerle dizi kullanımı
+            //Klavyeden girilen n tane sayının ortalamasını hesaplayan program
+            Console.WriteLine("Lütfen dizinin eleman sayısını giriniz: ");
+            int diziUzunlugu = int.Parse(Console.ReadLine());
+            int [] sayıDizisi = new int[diziUzunlugu];
+
+            for (int i = 0; i < diziUzunlugu; i++)
+            {
+                Console.Write("Lütfen {0}.sayısını giriniz: ",i+1);
+                sayıDizisi[i]= int.Parse(Console.ReadLine());
+
+            }
+            int toplam =0;
+            foreach (var sayi in sayıDizisi)
+            {
+                toplam+=sayi;
+            }
+            Console.WriteLine("Ortalama :" +toplam/diziUzunlugu);
+        }
+    }
+}
