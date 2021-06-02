f2958fe50606863a637c2dd0f849914a9c0991ea
aleynazengin <aleystyles23@gmail.com>
2021-06-02 18:57:22 +0300

    Çalışma1 ve çalışma2 eklendi

---
+++ Calisma1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Calisma1
+{
+    class Program
+    {
+          static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("İsminizi Giriniz: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyadınızı Giriniz: ");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Merhaba " + name + " " + surname);
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ Calisma2/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace Calisma2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte b = 5; //1 byte
+            sbyte c=  5; //1 byte
+
+            short s= 5; //2 byte
+            ushort us = 5; //2 byte
+
+            Int16 i16= 2; //2 byte
+            int i =2; //4 byte
+            Int32 i32= 2; //4 byte
+            Int64 i64= 2; //8  byte
+
+            uint ui =2 ; //4 byte 
+            long l = 4; //8 byte
+            ulong ul= 4; //8 byte
+
+            //Reel Sayılar
+            float f = 5; //4 byte
+            double d = 5; //8 byte
+            decimal de = 5; //16 byte
+
+            char ch = '2'; //2 byte
+            string str = "Aleyna"; //sınırsız
+
+            bool b1= true;
+            bool b2= false;
+
+            DateTime dt= DateTime.Now;
+            Console.WriteLine(dt);
+
+            object o1 = "x";
+            object o2= 'y';
+            object o3 = 4;
+            object o4 = 4.3;
+
+            //string ifadeler
+
+            string str1= string.Empty;
+            str1 = "Aleyna Zengin";
+            string ad = "Aleyna";
+            string soyad = "Zengin";
+            string tamIsim = ad + " " + soyad;
+
+            //Integer tanımlama şekilleri
+
+            int integer1= 5;
+            int integer2= 3;
+            int integer3= integer1+integer2;
+
+            //boolean
+            bool bool1 = 10>2;
+
+            //Değişken dönüşümleri
+            string str20 = "20";
+            int int20 = 20;
+            string yeniDeger = str20 + int20.ToString();
+            Console.WriteLine(yeniDeger); // Çıktısı 2020
+
+            int int21= int20 + Convert.ToInt32(str20);
+            Console.WriteLine(int21); // Çıktısı 40
+
+            int int22= int20 + int.Parse(str20); //Çıktısı 40
+
+            // datetime
+
+            string datetime = DateTime.Now.ToString("dd.MM.yyyy");
+            Console.WriteLine(datetime);
+
+            string datetime2 = DateTime.Now.ToString("dd/MM/yyyy");
+            Console.WriteLine(datetime2);
+
+            //saat
+            string hour = DateTime.Now.ToString("HH:mm");
+            Console.WriteLine(hour);
+
+
+
+        }
+    }
+}
