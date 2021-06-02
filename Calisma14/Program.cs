51a1e118d451baa93029474d93bed5ec6a1ea3b1
aleynazengin <aleystyles23@gmail.com>
2021-06-03 02:39:16 +0300

    Rekürsif ve Extension Metotlar

---
+++ Calisma14/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace Calisma14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //Rekürsif - Öz yinelemeli
+            // 3^4 = 3*3*3*3
+
+            int result = 1;
+            for (int i = 1; i < 5; i++)
+                result= result*3;   
+            Console.WriteLine(result);
+         
+            Islemler instance = new();
+            Console.WriteLine(instance.Expo(3,4));
+           
+            // Extension Metotlar
+            string ifade="Aleyna Zengin";
+            bool sonuc = ifade.CheckSpaces();
+            Console.WriteLine(sonuc);
+           
+            if(sonuc)
+                Console.WriteLine(ifade.RemoveWhiteSpaces());
+            
+            Console.WriteLine(ifade.MakeUpperCase());
+            Console.WriteLine(ifade.MakeLowerCase());
+
+            int[] dizi = {9,6,3,1,5,4};
+            dizi.SortArray();
+            dizi.EkranaYazdir();
+
+            int sayi=5;
+            Console.WriteLine(sayi.IsEven());
+            
+            Console.WriteLine(ifade.GetFirstCharacter());
+        }
+    }
+    public class Islemler{
+        public int Expo(int sayi,int üs)
+        {
+            if(üs<2)
+                return sayi;
+            return Expo(sayi,üs-1)*sayi;
+
+        }
+        //Expo(3,4)
+        //Expo(3,3)*3
+        //Expo(3,2)*3*3
+        //Expo(3,1)*3*3*3
+        // 3*3*3*3 = 3^4
+    }
+    public static class Extension {
+            public static bool CheckSpaces(this string param)
+            {
+                return param.Contains(" ");
+            }
+             public static string RemoveWhiteSpaces(this string param)
+            {
+                string[] dizi = param.Split(" "); 
+                return string.Join("*",dizi); //dizideki kelimeleri aralarına * koyarak döndürdük.
+            }
+
+             public static string MakeUpperCase(this string param)
+            {
+                return param.ToUpper();
+            }
+            public static string MakeLowerCase(this string param)
+            {
+                return param.ToLower();
+            }
+            public static int[] SortArray(this int[] param)
+            {
+                Array.Sort(param);
+                return param;
+            }
+            public static void EkranaYazdir(this int[] param)
+            {
+                foreach (int item in param)
+                    Console.WriteLine(item);
+            }
+            public static bool IsEven (this int number){
+                return number%2==0;
+            }
+
+            public static string GetFirstCharacter (this string param)
+            {
+                return param.Substring(0,1); // 0.indexten başlayıp 1 karakter getir
+            }
+
+
+    }
+
+}
