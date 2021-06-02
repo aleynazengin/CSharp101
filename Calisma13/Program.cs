61a64ccdb85073a76d9e221ba34bcd497fc411f8
aleynazengin <aleystyles23@gmail.com>
2021-06-02 21:49:59 +0300

    Metot Overloading

---
+++ Calisma13/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Calisma13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            //out parametreler
+            string sayi = "999";
+            bool sonuc = int.TryParse(sayi, out int outSayi);
+            if(sonuc)
+            {
+                Console.WriteLine("Başarılı!");
+                Console.WriteLine(outSayi);
+            }
+            else{
+                Console.WriteLine("Başarısız!");
+            }
+            Metotlar instance = new Metotlar();
+            instance.Topla(4,5,out int toplamSonucu);
+            Console.WriteLine(toplamSonucu);
+
+            //Metot aşırı yükleme - Overloading
+            int ifade = 999;
+            instance.EkranaYazdir(Convert.ToString(ifade));
+            instance.EkranaYazdir(ifade);
+            instance.EkranaYazdir("Aleyna","Zengin");
+
+            //Metot İmzası
+            //metotAdı + parametre sayisi + parametre
+            
+
+        }
+    }
+    class Metotlar {
+        public void Topla (int a, int b,out int toplam)
+        {
+            toplam = a+b;
+        }
+
+        public void EkranaYazdir (string veri)
+        {
+            Console.WriteLine(veri);
+        }
+
+         public void EkranaYazdir (int veri)
+        {
+            Console.WriteLine(veri);
+        }
+         public void EkranaYazdir (string veri1,string veri2)
+        {
+            Console.WriteLine(veri1+veri2);
+        }
+
+
+    }
+}
