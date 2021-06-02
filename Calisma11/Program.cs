5eb73e78ec64bfeb92c5ea606fb2381193adf5fe
aleynazengin <aleystyles23@gmail.com>
2021-06-02 20:17:09 +0300

    Array Sınıfı Methodları

---
+++ Calisma11/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Calisma11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sort
+            int [] sayiDizisi = {23,12,4,87,72,3,11,17};
+
+            Console.WriteLine("*** Sırasız Dizi ***");
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+            Console.WriteLine("*** Sıralı Dizi ***");
+            Array.Sort(sayiDizisi);
+
+             foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+
+            //Clear
+            Console.WriteLine("*** Array Clear ***");
+            //sayiDizisi elemanlarını kullanarak 2.indexten itibaren 2 tane elemanı 0'lar.
+            Array.Clear(sayiDizisi,2,2);
+
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+            //Reverse
+            Console.WriteLine("*** Array Reverse ***");
+            Array.Reverse(sayiDizisi);
+
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+            
+            //IndexOf
+            Console.WriteLine("*** Array IndexOf ***");
+             //23 değerinin kaçıncı indexte olduğunu bul
+            Console.WriteLine(Array.IndexOf(sayiDizisi,23));
+
+            //Resize : Yeniden boyutlandırma
+            Console.WriteLine("*** Array Resize ***");
+            Array.Resize<int>(ref sayiDizisi,9); //8 elemanlı dizimizi 9 elemanlı yap.
+            sayiDizisi[8]= 99;
+
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+
+        }
+    }
+}
